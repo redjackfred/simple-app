785a8846b68d21b311d0fa60d3bbbd01e633ea92
redjackfred <redjackfred@gmail.com>
2025-08-21 15:34:51 -0700

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<div className="flex flex-col items-center justify-center h-screen">
+			<label htmlFor="name">Name</label>
+			<input type="text" id="name" name="name" />
+			<label htmlFor="email">Email</label>
+			<input type="email" id="email" name="email" />
+			<label htmlFor="college-level">BS</label>
+			<input type="radio" id="college-level" name="education" value="BS" />
+			<label htmlFor="college-level">MS</label>
+			<input type="radio" id="college-level" name="education" value="MS" />
+			<label htmlFor="classes-taken">Classes Taken</label>
+			<input type="text" id="classes-taken" name="classes-taken" />
+			<label htmlFor="class-expectation">Class Expectations</label>
+			<input type="textfield" id="classes-taken" name="classes-taken" />
+		</div>
+	);
 }
 
-export default App
+export default App;
